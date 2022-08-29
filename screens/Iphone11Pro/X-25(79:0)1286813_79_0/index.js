22b697ce7738b3017c724040dd308c57863a2483
Crowdbotics <team@crowdbotics.com>
2022-08-29 07:48:41 +0000

    Update components

---
+++ screens/Iphone11Pro/X-25(79:0)1286813_79_0/index.js
@@ -0,0 +1,72 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_79_138}>
+        <Text style={styles.Text_79_138}>Welcome!</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("111%") },
+  View_79_138: {
+    width: wp("43%"),
+    minWidth: wp("43%"),
+    minHeight: hp("5%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("29%"),
+    top: hp("53%"),
+    justifyContent: "flex-start"
+  },
+  Text_79_138: {
+    color: "rgba(20, 20, 20, 1)",
+    fontSize: 26,
+    fontWeight: "900",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
